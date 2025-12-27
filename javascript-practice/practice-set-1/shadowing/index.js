a3ad527f65de10310b927c197997fff38e69e197
AdarshTiwari3 <a.tiwariadarsh99@gmail.com>
2025-12-27 15:40:01 +0530

    feat: add the example of variable overshadowing of `var`

--- javascript-practice/practice-set-1/shadowing/index.js
+++ javascript-practice/practice-set-1/shadowing/index.js
@@ -1,19 +1,19 @@
-let y=10
+let y = 10;
+var x = 11;
 {
-    var x=1
-    let y=2
-    const z=10 
-    console.log("Inside Block")
-    console.log(x)
-    console.log(y)
-    console.log(z)
+  var x = 1;
+  let y = 2;
+  const z = 10;
+  console.log("Inside Block");
+  console.log(x);
+  console.log(y);
+  console.log(z);
 }
 
-console.log("Outside Block")
-console.log(x)
-console.log(y)
-console.log(z)
-
+console.log("Outside Block");
+console.log(x);
+console.log(y);
+console.log(z);
 
 /*output:
 Inside Block
@@ -27,4 +27,5 @@ index.js:15 Uncaught ReferenceError: z is not defined
     at index.js:15:13
 
 because let maintains a different block scope everytime
+why value of x is still 1, because var usage global scope and even if you create inside scope {} it will still point to global only hence value is changed to 1
 */
