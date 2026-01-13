e4cd4f511e134120bd426ba257468cc36fb7647d
AdarshTiwari3 <a.tiwariadarsh99@gmail.com>
2026-01-13 10:11:44 +0530

    feat: add example code of use-effect hook in react

--- react-tailwind-practice/core-react/src/topics/hooks/index/hooks-route.tsx
+++ react-tailwind-practice/core-react/src/topics/hooks/index/hooks-route.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import HooksIndex from "./hooks-index";
 import UseStateDemo from "@/topics/hooks/use-state-hook/use-state-demo";
+import UseEffectDemo from "@/topics/hooks/use-effect-hook/use-effect-demo";
 
 const HooksRoutes = () => {
   return (
     <Routes>
       <Route index element={<HooksIndex />} />
       <Route path="use-state" element={<UseStateDemo />} />
+      <Route path="use-effect" element={<UseEffectDemo />} />
     </Routes>
   );
 };
