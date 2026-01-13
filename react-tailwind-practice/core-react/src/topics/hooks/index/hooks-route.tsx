8ca6b718da834787763fe9597e8030ce7f405222
AdarshTiwari3 <a.tiwariadarsh99@gmail.com>
2026-01-13 11:06:13 +0530

    feat: add use-memo hook example

--- react-tailwind-practice/core-react/src/topics/hooks/index/hooks-route.tsx
+++ react-tailwind-practice/core-react/src/topics/hooks/index/hooks-route.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import HooksIndex from "./hooks-index";
 import UseStateDemo from "@/topics/hooks/use-state-hook/use-state-demo";
 import UseEffectDemo from "@/topics/hooks/use-effect-hook/use-effect-demo";
+import UseMemoDemo from "@/topics/hooks/use-memo-hook/use-memo-demo";
 
 const HooksRoutes = () => {
   return (
@@ -9,6 +10,7 @@ const HooksRoutes = () => {
       <Route index element={<HooksIndex />} />
       <Route path="use-state" element={<UseStateDemo />} />
       <Route path="use-effect" element={<UseEffectDemo />} />
+      <Route path="use-memo" element={<UseMemoDemo />} />
     </Routes>
   );
 };
