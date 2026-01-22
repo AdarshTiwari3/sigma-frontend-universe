a3fd5f34655065a99b39b3113775680888603afa
AdarshTiwari3 <a.tiwariadarsh99@gmail.com>
2026-01-22 19:33:12 +0530

    feat: add routing for zustand store

--- react-tailwind-practice/core-react/src/router/app-router.tsx
+++ react-tailwind-practice/core-react/src/router/app-router.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ReactTopics from "@/pages/react-topics";
 import HooksRoutes from "@/topics/hooks/index/hooks-route";
+import ZustandStore from "@/topics/store/zustand";
 
 const AppRouter = () => {
   return (
@@ -8,6 +9,7 @@ const AppRouter = () => {
       <Routes>
         <Route path="/" element={<ReactTopics />} />
         <Route path="hooks/*" element={<HooksRoutes />} />
+        <Route path="zustand-store" element={<ZustandStore />} />
       </Routes>
     </BrowserRouter>
   );
