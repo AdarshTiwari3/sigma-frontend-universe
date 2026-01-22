b1447047054c66570516a33236b5e3f13b0ab9dd
AdarshTiwari3 <a.tiwariadarsh99@gmail.com>
2026-01-22 10:10:24 +0530

    feat: add custom hook for debouncing the width value to improve the performance

--- react-tailwind-practice/core-react/src/topics/hooks/custom-hook/display-window-board.tsx
+++ react-tailwind-practice/core-react/src/topics/hooks/custom-hook/display-window-board.tsx
@@ -1,14 +1,19 @@
+import { useDebounce } from "./use-debounce";
 import { useMobile } from "./use-mobile";
 import { useWindowWidth } from "./use-window-width";
 
 const DisplayWindowSizeBoard = () => {
   const width = useWindowWidth();
+  const debouncedWidth = useDebounce(width, 150);
   const isMobile = useMobile();
+  const isMobileDebounced = debouncedWidth < 768;
 
   return (
     <div className="p-8">
       <h2>Screen Width: {width}</h2>
+      <h2>Debounced Width:{debouncedWidth}</h2>
       <p>Is mobile screen: {isMobile ? "true" : "false"}</p>
+      <p>Is mobile screen: {isMobileDebounced ? "true" : "false"}</p>
     </div>
   );
 };
