23aa122073279fc9b7d6a6414775d9f5ec75b225
AdarshTiwari3 <a.tiwariadarsh99@gmail.com>
2026-01-22 19:32:54 +0530

    refactor: add map for nav link

--- react-tailwind-practice/core-react/src/pages/react-topics.tsx
+++ react-tailwind-practice/core-react/src/pages/react-topics.tsx
@@ -1,18 +1,31 @@
 import { Link } from "react-router-dom";
 
+type Topic = {
+  label: string;
+  path: string;
+};
+
+const topics: Topic[] = [
+  { label: "Hooks in React", path: "/hooks" },
+  { label: "Zustand Store", path: "/zustand-store" },
+];
+
 const ReactTopics = () => {
   return (
     <div>
       <div className="border-b border-cyan-700 p-3">
-        <h2 className="text-center"> Frontend Topics</h2>
+        <h2 className="text-center">Frontend Topics</h2>
       </div>
+
       <nav className="p-4">
-        <ol className="list-decimal list-inside">
-          <li>
-            <Link to="/hooks" className="hover:text-cyan-700 font-medium">
-              Hooks in React
-            </Link>
-          </li>
+        <ol className="list-decimal list-inside space-y-2">
+          {topics.map((topic) => (
+            <li key={topic.path}>
+              <Link to={topic.path} className="hover:text-cyan-700 font-medium">
+                {topic.label}
+              </Link>
+            </li>
+          ))}
         </ol>
       </nav>
     </div>
