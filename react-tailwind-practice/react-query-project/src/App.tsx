d8213c55452b102aa0e5bb94d23f57a1c1fe96cf
AdarshTiwari3 <a.tiwariadarsh99@gmail.com>
2026-02-23 17:46:09 +0530

    Initial Code Setup Commit

---
+++ react-tailwind-practice/react-query-project/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="w-full h-screen bg-slate-600">
+      <p className="text-center p-2">React Query Course</p>
+    </div>
+  );
+}
+
+export default App;
