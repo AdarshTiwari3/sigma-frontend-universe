ce4d0d79af780f98be35a5bc8f14eaedede923a4
AdarshTiwari3 <a.tiwariadarsh99@gmail.com>
2026-02-25 00:30:36 +0530

    build: add a bundle visualizer configuaration

--- react-tailwind-practice/react-query-project/vite.config.ts
+++ react-tailwind-practice/react-query-project/vite.config.ts
@@ -2,9 +2,19 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
+import { visualizer } from "rollup-plugin-visualizer";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    visualizer({
+      open: true,
+      filename: "dist/stats.html",
+      gzipSize: true,
+      brotliSize: true,
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
